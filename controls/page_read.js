07073738fb74ba40f2af77826132d5348160b15c
MOON <joamoon7@nexchal.co.kr>
2019-10-08 16:48:45 +0900

    상세보기 구현

---
+++ controls/page_read.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var qs = require('querystring');
+var oracledb=require('oracledb');
+var express=require('express');
+var dbConfig = require('./../config/dbconfig2.js');
+module.exports = {
+	HTML:function(req, res,title){
+      var arr = new Array(" ");
+			var oracledb = require('oracledb');
+			var dbConfig = require('./../config/dbconfig2.js');
+      oracledb.getConnection(dbConfig,function(err, conn)
+
+      {
+        conn.execute(`SELECT emp_name,area,area_1,area_2,id,emp_tel,emp_no FROM test_userinfo where emp_no = '${title}'`, function (err, result)
+        {
+          var name = result.rows[0][0];
+          var area = result.rows[0][1];
+          var area1 = result.rows[0][2];
+          var area2 = result.rows[0][3];
+          var id = result.rows[0][4];
+          var tel = result.rows[0][5];
+          var title = result.rows[0][6];
+
+          conn.execute(`SELECT LOGICNAME FROM FAULTLOGIC WHERE LOGICID IN(SELECT fault_logicid FROM TEST_ERR_TYPE where id = '${id}')`, function (err, logic)
+          {
+            var logic_name_val = logic.rows;
+            var logic_name_len = logic.rows.length;
+
+            res.render('title',
+            {
+              id: id, name : name, area : area, area1: area1, area2: area2,
+              two: logic_name_val, length: logic_name_len, tel : tel , title: title
+            });
+          });
+        });
+      });
+    }
+  }
--- models/listrouter.js
+++ models/listrouter.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 oracledb.autoCommit = true;
 var router = express.Router();
 var page;
+var arr = '';
 router.use(bodyParser.urlencoded({ extended: false }));
 
 
@@ -24,28 +25,8 @@ router.get('/list',function(req, res) //index
 router.get('/page/:pageId',function(req, res)
 {
   var title = req.params.pageId;
-
-  conn.execute(`SELECT emp_name,area,area_1,area_2,id,emp_tel,emp_no FROM test_userinfo where emp_no = '${title}'`, function (err, result)
-  {
-    var name = result.rows[0][0];
-    var area = result.rows[0][1];
-    var area1 = result.rows[0][2];
-    var area2 = result.rows[0][3];
-    var id = result.rows[0][4];
-    var tel = result.rows[0][5];
-    var title = result.rows[0][6];
-
-    conn.execute(`SELECT logicname FROM FAULTLOGIC where LOGICID = '${id}'`, function (err, ertable)
-    {
-      console.log(ertable.rows.length);
-      console.log(ertable.rows)
-      res.render('title',
-      {
-        id: id, name : name, area : area, area1: area1, area2: area2,
-        two: ertable.rows, length: ertable.rows.length, tel : tel , title: title
-      });
-    });
-  });
+  page = require('../controls/page_read.js');
+  return page.HTML(req,res,title);
 });
 
 
